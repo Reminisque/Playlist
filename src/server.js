b98955d9432d2f4b73877f4e3116f1ad29f79f13
Travis Chang <travisc2728@gmail.com>
2018-05-08 16:43:04 -0700

    Added server side code and node package file

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+// fs.readdir(path.join(__dirname, "/public/playlist"), function(err, items) {
+//     console.log(items);
+
+//     for (var i=0; i<items.length; i++) {
+//         console.log(items[i]);
+//     }
+// })
+
+
+var port = 5000;
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+// Get playlist
+// All requests routed to this path will be returned an array
+// containing the names of all songs currently stored on the server
+app.get("/playlist", function (req, res) {
+    fs.readdir(path.join(__dirname, "/playlist"), function (err, items) {
+        // for (var i=0; i<items.length; i++) {
+        //     items[i] = path.parse(items[i]).name;
+        // }
+        res.send(items);
+    });
+});
+
+// Get song
+// All requests routed to this path will be streamed
+// the song identified by parameter songId
+app.get("/song/:songId", function (req, res) {
+    songPath = path.join(__dirname, "/playlist", req.params.songId);
+    fs.createReadStream(songPath).pipe(res);
+})
+
+app.listen(port, function () {
+    console.log("Connected to port 5000. Welcome to Playlist!");
+});
